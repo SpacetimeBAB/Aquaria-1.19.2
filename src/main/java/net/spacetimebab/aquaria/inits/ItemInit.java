3ade26a73daf6643acda33473b67f03fc335bf24
Alpha-and-Gecc <86210245+Alpha-and-Gecc@users.noreply.github.com>
2022-09-17 21:27:14 -0700

    Add all current extinct soul crystals(will do latimeria and neoceratodus too you just wait)
    added crystals, nothing about them just yet

--- src/main/java/net/spacetimebab/aquaria/inits/ItemInit.java
+++ src/main/java/net/spacetimebab/aquaria/inits/ItemInit.java
@@ -22,13 +22,10 @@ public class ItemInit {
             DeferredRegister.create(ForgeRegistries.ITEMS, Aquaria.MOD_ID);
 
     public static final RegistryObject<Item> RAW_SPHETHACANTHUS = ITEMS.register("raw_sphenacantus",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.RAW_SPHETHACANTHUS)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.RAW_SPHETHACANTHUS)));
 
     public static final RegistryObject<Item> COOKED_SPHETHACANTHUS = ITEMS.register("cooked_sphenacantus",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.COOKED_SPHETHACANTHUS)));
-
-    public static final RegistryObject<Item> SPHENA_SOUL = ITEMS.register("sphena_soul",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.COOKED_SPHETHACANTHUS)));
 
     public static final RegistryObject<Item> SPHENA_BUCKET = ITEMS.register("sphena_bucket",
             () -> new ItemModFishBucket(EntityInit.SPHENACANTHUS, () -> Fluids.WATER, Items.BUCKET, false,
@@ -51,22 +48,22 @@ public class ItemInit {
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static final RegistryObject<Item> RAW_PHLEBOLEPIS = ITEMS.register("raw_phlebolepis",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.RAW_PHLEBOLEPIS)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.RAW_PHLEBOLEPIS)));
 
     public static final RegistryObject<Item> COOKED_PHLEBOLEPIS = ITEMS.register("cooked_phlebolepis",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.COOKED_PHLEBOLEPIS)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.COOKED_PHLEBOLEPIS)));
 
     public static final RegistryObject<Item> DOLLY_FLESH = ITEMS.register("dolly_flesh",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.RAW_DOLI)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.RAW_DOLI)));
 
     public static final RegistryObject<Item> DOLLY_FLESH_COOKED = ITEMS.register("dolly_flesh_cooked",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.COOKED_DOLI)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.COOKED_DOLI)));
 
     public static final RegistryObject<Item> RAW_GOOLAGONGIA = ITEMS.register("raw_goolagongia",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.RAW_GOOLAGONGIA)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.RAW_GOOLAGONGIA)));
 
     public static final RegistryObject<Item> COOKED_GOOLAGONGIA = ITEMS.register("cooked_goolagongia",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).food(FoodInit.COOKED_GOOLAGONGIA)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodInit.COOKED_GOOLAGONGIA)));
 
     public static final RegistryObject<Item> GOOLAGONGIA_BUCKET = ITEMS.register("goolagongia_bucket",
             () -> new ItemModFishBucket(EntityInit.GOOLOGONGIA, () -> Fluids.WATER, Items.BUCKET, false,
@@ -79,10 +76,44 @@ public class ItemInit {
     public static final RegistryObject<Item> CAMPBELLODUS_SPAWN_EGG = ITEMS.register("campbellodus_spawn_egg",
             () -> new ForgeSpawnEggItem(EntityInit.CAMPBELLODUS, 0xDFCC8F, 0x2D2611,
                     new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    
+    //new items below, pls sort and point out faults :]
+    
+    public static final RegistryObject<Item> SPHENA_CRYSTAL = ITEMS.register("sphena_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> GOOGOO_CRYSTAL = ITEMS.register("googoo_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> ORNITHOPRION_CRYSTAL = ITEMS.register("ornithoprion_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+    public static final RegistryObject<Item> DOLLY_CRYSTAL = ITEMS.register("dolly_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    
 
+    public static final RegistryObject<Item> DIPLACANTHUS_CRYSTAL = ITEMS.register("diplacanthus_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> DIPTERUS_CRYSTAL = ITEMS.register("dipterus_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> CAMPBELLODUS_CRYSTAL = ITEMS.register("campbellodus_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    public static final RegistryObject<Item> LAMIASPIS_CRYSTAL = ITEMS.register("dipterus_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    public static final RegistryObject<Item> BUNGARTIUS_CRYSTAL = ITEMS.register("bungartius_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    
+    public static final RegistryObject<Item> PHLEBOLEPIS_CRYSTAL = ITEMS.register("phlebolepis_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    
+    
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
